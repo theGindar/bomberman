16b5dfaf20644f28b98d4fcb9c2f1b5acc045dac
Patrick Guenther <patrick.guenther01@sap.com>
2021-03-12 19:48:39 +0100

    reduce coins

--- agent_code/my_agent/train.py
+++ agent_code/my_agent/train.py
@@ -39,7 +39,7 @@ LEARNING_RATE = 0.0001
 target_net = Model().to(device)
 policy_net = Model().to(device)
 
-if len(os.listdir("./agent_code/my_agent/saved_models")) == 0:
+if len(os.listdir("./agent_code/my_agent/saved_models")) != 0:
     print('loading existing model...')
     policy_net.load_state_dict(torch.load("./agent_code/my_agent/saved_models/krasses_model.pt", map_location=torch.device('cpu')))
     #policy_net.load_state_dict(torch.load("./agent_code/my_agent/saved_models/krasses_model.pt"))

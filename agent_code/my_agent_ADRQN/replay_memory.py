fix wrong index in sample

agent_code/my_agent_ADRQN/replay_memory.py
@@ -110,9 +110,10 @@ class ReplayMemorySameEpisode(object):
                     break
                 else:
                     too_short_episodes.append(episode_idx)
-                    too_short_episodes.sort(reverse=True)
-                    for idx in too_short_episodes:
-                        self.storage[idx].pop(idx)
+
+            too_short_episodes = too_short_episodes.sort(reverse=True)
+            for idx in set(too_short_episodes):
+                self.storage[idx].pop(idx)
 
         return torch.tensor(last_actions).to(device), \
                torch.stack(last_observations).permute(0, 2, 1, 3, 4), \

agent_code/my_agent_ADRQN/train.py
@@ -538,7 +538,11 @@ def optimize_model_depr(self):
 def optimize_model(self):
     if len(self.memory) <= BATCH_SIZE:
         return
-    last_actions, last_observations, actions, rewards, observations, dones = self.memory.sample(BATCH_SIZE)
+
+    if USE_EPISODE_MEMORY:
+        last_actions, last_observations, actions, rewards, observations, dones = self.memory.sample(BATCH_SIZE)
+    else:
+        last_actions, last_observations, actions, rewards, observations, dones = self.memory.sample(BATCH_SIZE, SEQ_LEN)
 
     # Pass the sequence of last observations and actions through the network
     q_values, _ = policy_net.forward(last_observations, F.one_hot(last_actions, 6).float())